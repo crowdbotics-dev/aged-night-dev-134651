3cd80f1a4dbd1db2648b868bf531a5519b3dc20d
Crowdbotics <engineering@crowdbotics.com>
2024-03-26 18:26:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-night-dev-134651",
+  url: "https://aged-night-dev-134651.botics.co",
+  api: "https://aged-night-dev-134651.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
